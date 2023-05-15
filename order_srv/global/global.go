0bf04ae89afcd3550e679f05159792a70de9ea0a
DESKTOP-RQA0AM7\admin <584268914@qq.com>
2023-05-15 22:45:14 +0800

    rocketmq

---
+++ order_srv/global/global.go
@@ -0,0 +1,12 @@
+package global
+
+import (
+	"gorm.io/gorm"
+	"mxshop_srvs/goods_srv/config"
+)
+
+var (
+	DB           *gorm.DB
+	ServerConfig config.ServerConfig
+	NacosConfig  config.NacosConfig
+)
---
+++ order_srv/model/main/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+	"log"
+	"mxshop_srvs/goods_srv/model"
+	"os"
+	"time"
+)
+
+func main() {
+	dsn := "root:root@tcp(127.0.0.1:3306)/shop_goods_srv?charset=utf8mb4&parseTime=True&loc=Local"
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold: time.Second,
+			LogLevel:      logger.Info,
+			Colorful:      true,
+		},
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
+		NamingStrategy: schema.NamingStrategy{ //user model 就是user表不加s
+			SingularTable: true,
+		},
+		Logger: newLogger,
+	})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&model.Category{}, model.Brands{}, model.Banner{}, model.GoodsCategoryBrand{}, model.Goods{})
+}
